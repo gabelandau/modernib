8230283339f60b2f3bfd2588128866eced616bd6
gabelandau <me@gabelandau.com>
2019-08-21 18:06:33 -0400

    Added home page and implemented router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,14 @@
 import React from 'react'
-import './App.scss'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from '../../pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   )
 }
 
